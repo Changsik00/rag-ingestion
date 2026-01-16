7487debd86c8ab07fa95cc747981200fed01a7ac
changsik <lowmans00@gmail.com>
2026-01-16 19:18:49 +0900

    refactor: update imports to langchain_core for 0.3 compatibility

--- app/domain/services/semantic_extractor.py
+++ app/domain/services/semantic_extractor.py
@@ -1,7 +1,7 @@
 from typing import Optional
 import logging
-from langchain.prompts import PromptTemplate
-from langchain.output_parsers import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.exceptions import OutputParserException
 from langchain_google_genai import ChatGoogleGenerativeAI
 
